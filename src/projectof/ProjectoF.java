5d9c44fe3ef726d3c296b318c560652c4ed2b772
paulipauli99 <50640534+paulipauli99@users.noreply.github.com>
2019-05-17 17:00:14 +0100

    Adicionei a class reanto

--- src/projectof/ProjectoF.java
+++ src/projectof/ProjectoF.java
@@ -5,8 +5,10 @@
  */
 package projectof;
 
+import Frontend.Projetos;
 import backend.FileIO;
 import backend.Projeto;
+import backend.Tarefa;
 import backend.Utilizador;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -26,17 +28,26 @@ public class ProjectoF {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        ArrayList<Projeto> listaProjetos;
+        ArrayList<Projeto> projetos;
         ArrayList<Utilizador> utilizadores;
+        ArrayList<Tarefa> tarefas;
         
         // TODO code application logic here
-        listaProjetos = new ArrayList<>();
+        projetos = new ArrayList<>();
         utilizadores = new ArrayList<>();
+        tarefas = new ArrayList<>();
         
+        //testes de utilizadores
         Utilizador user1 = new Utilizador("joao", "jeje", "user1@mail.com", "pass1"); 
         utilizadores.add(user1);
         Utilizador user2 = new Utilizador("paulo", "user2", "user2@mail.com", "pass2"); 
         utilizadores.add(user2);
+        
+        //testes de projetos
+       Projeto proj1 = new Projeto("visaox");
+       projetos.add(proj1);
+       Projeto proj2 = new Projeto("visaoy");
+       projetos.add(proj2);
         
         FileIO io = new FileIO();
         io.saveUsers(utilizadores);
@@ -44,7 +55,11 @@ public class ProjectoF {
         FileIO read = new FileIO();
         read.readUsers(utilizadores);
         
-        System.out.println(nome+","+username+","+email","+pass";" );
+        FileIO io1 = new FileIO();
+        io1.saveProj(projetos);
+        
+        
+        System.out.println();
     }
     
 }
